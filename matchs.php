178a8c23c015d6dcc25267ced5ce732ecf183510
GeneralLemmon <tom.drs09@gmail.com>
2026-06-03 10:46:30 +0200

    Grandes lignes terminées

--- matchs.php
+++ matchs.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr-FR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,14 +17,18 @@
     <div class="matchs-container">
         <div class="match-card">
             <p>14 juin 2026 - 18h</p>
-            <img src="Images/lieu.png" alt="Lieu" width="50"><p> Puteaux Île</p>
-            <img src="Images/player.png" alt="Joueurs" width="50"><p> 2/4 Joueurs</p>
-            <img src="Images/" alt="Niveau" width="50"><p> Niveau moyen</p>
+            <img src="Images/lieu.png" alt="Lieu" width="50">
+            <p> Puteaux Île</p>
+            <img src="Images/player.png" alt="Joueurs" width="50">
+            <p> 2/4 Joueurs</p>
+            <img src="Images/level.png" alt="Niveau" width="50">
+            <p> Niveau moyen : 3.7</p>
         </div>
     </div>
 
     <img src="Images/notification.png" alt="Logo Notification" class="logo" width="50">
     <?php require "footer.php"; ?>
     <script src="script.js"></script>
-</bo</html>
-dy>
+</body>
+
+</html>
