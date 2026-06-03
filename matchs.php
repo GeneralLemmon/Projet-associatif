5d6fecf512e62effd2afcd0dc72c0b937048315e
GeneralLemmon <tom.drs09@gmail.com>
2026-06-03 10:24:11 +0200

    Update matchs

--- matchs.php
+++ matchs.php
@@ -13,8 +13,16 @@
 
     <h2>Mes prochains matchs</h2>
 
-    
+    <div class="matchs-container">
+        <div class="match-card">
+            <p>14 juin 2026 - 18h</p>
+            <p>Lieu: Puteaux Île</p>
+            <p>4/4 Joueurs</p>
+            <p>Niveau moyen</p>
+        </div>
+    </div>
 
+    <img src="Images/notification.png" alt="Logo Notification" class="logo" width="50">
     <?php require "footer.php"; ?>
     <script src="script.js"></script>
 </body>
